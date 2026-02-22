4298de120d210047855cd016da5e134c716707c3
Daniel Aaron Gamez Barcelo <lelekun5@outlook.com>
2026-02-21 18:39:10 -0700

    feat: integrate apicountries to cascading dropdowns and borders table

--- src/app/country/pages/country-page/country-page.component.ts
+++ src/app/country/pages/country-page/country-page.component.ts
@@ -1,11 +1,68 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { CountryService } from '../../services/country.service';
+import { Region, SmallCountry } from '../../interfaces/country.interfaces';
+import { filter, switchMap, tap } from 'rxjs';
 
 @Component({
   selector: 'app-country-page',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './country-page.component.html',
   styles: ``
 })
-export class CountryPageComponent {
+export class CountryPageComponent implements OnInit {
+
+  private fb = inject(FormBuilder);
+  private countryService = inject(CountryService);
+
+  public myForm: FormGroup = this.fb.group({
+    region: ['', [ Validators.required ]],
+    country: ['', [ Validators.required ]],
+    border: ['', [ Validators.required ]],
+  });
+
+  public regions: Region[] = [];
+  public countriesByRegion: SmallCountry[] = [];
+  public borders: SmallCountry[] = [];
+  
+  // New property for the table
+  public borderCountries: SmallCountry[] = []; 
+
+  ngOnInit(): void {
+    this.regions = this.countryService.regions;
+
+    this.onRegionChanged();
+    this.onCountryChanged();
+  }
+
+  onRegionChanged(): void {
+    this.myForm.get('region')!.valueChanges
+      .pipe(
+        tap( () => this.myForm.get('country')!.setValue('') ),
+        tap( () => this.borders = [] ),
+        tap( () => this.borderCountries = [] ),
+        switchMap( (region) => this.countryService.getCountriesByRegion( region ) ),
+      )
+      .subscribe( countries => {
+        this.countriesByRegion = countries.sort((a,b) => a.name.localeCompare(b.name));
+      });
+  }
+
+  onCountryChanged(): void {
+    this.myForm.get('country')!.valueChanges
+      .pipe(
+        tap( () => this.myForm.get('border')!.setValue('') ),
+        tap( () => this.borderCountries = [] ),
+        filter( (value: string) => value.length > 0 ),
+        switchMap( (alphaCode) => this.countryService.getCountryByAlphaCode( alphaCode ) ),
+        switchMap( (country) => this.countryService.getCountryBordersByCodes( country.borders ) )
+      )
+      .subscribe( countries => {
+        this.borders = countries;
+        this.borderCountries = countries; // store for the table
+      });
+  }
 
 }
